5acd951efd0b5226f0165c70e85ee4d8c9638046
Xilosof <pro100temi444@gmail.com>
2021-09-01 20:07:10 +0300

    csharp/pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+public static class Pangram
+{
+    private static List<char> Alphabet = new List<char>()
+    {
+        'q','w','e','r','t','y','u','i','o','p',
+        'a','s','d','f','g','h','j','k','l',
+        'z','x','c','v','b','n','m',
+    };
+    public static bool IsPangram(string input)
+    {
+        var tmpAlphabet = Alphabet.ToList();
+
+        foreach (var c in input.ToLower())
+            if (tmpAlphabet.Contains(c))
+                tmpAlphabet.Remove(c);
+
+        return tmpAlphabet.Count == 0;
+    }
+}
