5ffb6fd947f857ecdf8a37ed05d1d7204e444cbc
Xilosof <pro100temi444@gmail.com>
2021-09-03 11:51:34 +0300

    csharp/cars-assemble upload solution

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -3,12 +3,15 @@ using System;
 static class AssemblyLine
 {
     public static double ProductionRatePerHour(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
-    }
+        => speed switch
+        {
+            5 or 6 or 7 or 8 => speed * 221 * 0.9,
+            9 => speed * 221 * 0.8,
+            10 => speed * 221 * 0.77,
+            _ => speed * 221,
+        };
+
 
     public static int WorkingItemsPerMinute(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    }
+        => (int)ProductionRatePerHour(speed) / 60;
 }
