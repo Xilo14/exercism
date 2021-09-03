827756948ec8721ece20b5615c92a0ef735a1e5c
Xilosof <pro100temi444@gmail.com>
2021-09-03 22:48:23 +0300

    csharp/interest-is-interesting upload solution

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -1,19 +1,42 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 static class SavingsAccount
 {
     public static float InterestRate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
-    }
+        => balance switch
+        {
+            < 0 => -3.213f,
+            >= 0 and < 1000 => 0.5f,
+            >= 1000 and < 5000 => 1.621f,
+            >= 5000 => 2.475f
+        };
 
     public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+        => balance + balance * Math.Abs((decimal)InterestRate(balance)) / 100m;
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+        => Enumerable.Repeat(balance, 1)
+        .Select(b => new { ind = 0, val = b })
+        .Progression((prev) => new { ind = prev.ind + 1, val = AnnualBalanceUpdate(prev.val) })
+        .First((e) => e.val >= targetBalance).ind;
+
+}
+public static class Ext
+{
+    public static IEnumerable<T> Progression<T>
+        (this IEnumerable<T> source,
+         Func<T, T> progression)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        using var iterator = source.GetEnumerator();
+        if (!iterator.MoveNext())
+        {
+            yield break;
+        }
+        T newEl = iterator.Current;
+        while (true)
+            yield return newEl = progression(newEl);
     }
 }
