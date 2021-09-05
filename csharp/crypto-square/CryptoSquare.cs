e0f3f47d265e15c5d8bd08703f90eea41c1edf71
Xilosof <pro100temi444@gmail.com>
2021-09-05 08:14:16 +0300

    csharp/crypto-square upload solution

--- csharp/crypto-square/CryptoSquare.cs
+++ csharp/crypto-square/CryptoSquare.cs
@@ -1,25 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class CryptoSquare
 {
-    public static string NormalizedPlaintext(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static IEnumerable<char> NormalizedPlaintext(string plaintext)
+    => plaintext
+        .Where(c => char.IsLetterOrDigit(c))
+        .Select(c => char.ToLower(c));
 
-    public static IEnumerable<string> PlaintextSegments(string plaintext)
+    public static IEnumerable<string> PlaintextSegments(IEnumerable<char> plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        var l = plaintext.Count();
+        var c = (int)Math.Ceiling(Math.Sqrt(plaintext.Count()));
 
-    public static string Encoded(string plaintext)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+        var x = plaintext
+          .Concat(Enumerable.Repeat(' ',
+              l % c == 0 ? 0 : c - l % c));
+        return
+          x.SplitEvery(c);
     }
 
     public static string Ciphertext(string plaintext)
+        => (plaintext.Length == 0)
+            ? string.Empty
+            : string.Join(" ", PlaintextSegments(NormalizedPlaintext(plaintext))
+                .SelectMany(row => row.Select(
+                    (c, i) => new { c, i }))
+                .GroupBy(a => a.i)
+                .Select(g => string.Concat(g.Select(a => a.c))));
+
+
+    public static IEnumerable<string> SplitEvery(this IEnumerable<char> s, int n)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int index = 0;
+        return s.GroupBy(_ => index++ / n)
+                .Select(g => new string(g.ToArray()));
     }
 }
